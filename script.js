11cef0034326b1ef26363123fc163a603f7d4398
Steven Palfreyman <ironmonkus@gmail.com>
2022-01-09 15:47:25 -0500

    Initial push

---
+++ script.js
@@ -0,0 +1,30 @@
+const principal = document.getElementById("principal");
+const interest = document.getElementById("interest");
+const length = document.getElementById("length");
+const submit = document.getElementById("submit");
+const total = document.getElementById("total");
+
+submit.addEventListener("click", function () {
+  if (principal.value !== "" && interest.value !== "" && length.value !== "") {
+    let p = principal.value;
+    let r = interest.value / 100 / 12;
+    let n = length.value * 12;
+
+    let tot = (r * p) / (1 - (1 + r) ** -n);
+    tot = Math.floor(tot);
+
+    let string = tot.toString();
+    let fish = string.length;
+
+    // ADD comma in the monthly mortgage payment
+    if (fish < 4) {
+      total.textContent = `$${string}`;
+    } else if (fish === 4) {
+      total.textContent = `$${string[0]},${string.slice(-3)}`;
+    } else if (fish === 5) {
+      total.textContent = `$${string[0]}${string[1]},${string.slice(-3)}`;
+    } else {
+      total.textContent = ` Not worth it!`;
+    }
+  }
+});
