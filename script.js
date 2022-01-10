4f6a90fcbd22d9c2eb2c29c6b70c2fe1bc8c93a7
Steven Palfreyman <ironmonkus@gmail.com>
2022-01-10 18:01:51 -0500

    Made changes from review
    toLocaleString
    button instead of input
    submit instead of click event handler

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 const principal = document.getElementById("principal");
 const interest = document.getElementById("interest");
 const length = document.getElementById("length");
-const submit = document.getElementById("submit");
+const mortgageCalculator = document.getElementById("mortgage-calculator");
 const total = document.getElementById("total");
 
-submit.addEventListener("click", function () {
-  if (principal.value > 0 && interest.value >= 0 && length.value > 0) {
-    let p = principal.value;
-    let r = interest.value / 100 / 12;
-    let n = length.value * 12;
+mortgageCalculator.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const principalAmount = principal.value;
+  const monthlyInterestRate = interest.value / 100 / 12;
+  const totalPayments = length.value * 12;
 
-    let tot = (r * p) / (1 - (1 + r) ** -n);
-    tot = Math.floor(tot);
-
-    let string = tot.toString();
-    let fish = string.length;
-
-    // ADD comma in the monthly mortgage payment
-    if (fish < 4) {
-      total.textContent = `$${string}`;
-    } else if (fish === 4) {
-      total.textContent = `$${string[0]},${string.slice(-3)}`;
-    } else if (fish === 5) {
-      total.textContent = `$${string[0]}${string[1]},${string.slice(-3)}`;
-    } else {
-      total.textContent = ` Not worth it!`;
-    }
-  }
+  let monthlyMortgagePayment =
+    (monthlyInterestRate * principalAmount) /
+    (1 - (1 + monthlyInterestRate) ** -totalPayments);
+  monthlyMortgagePayment = monthlyMortgagePayment.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 0,
+  });
+  total.textContent = monthlyMortgagePayment;
 });
