b8f94197313ee9256315367a39b2a368abbfb540
Steven Palfreyman <ironmonkus@gmail.com>
2022-01-11 11:26:51 -0500

    Update script.js
    Added a check for 0 on principal and length
    Added a calculation for 0 interest

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
+// DOM connector
 const principal = document.getElementById("principal");
-const interest = document.getElementById("interest");
-const length = document.getElementById("length");
+const interestRate = document.getElementById("interest");
+const lengthOfLoan = document.getElementById("length");
 const mortgageCalculator = document.getElementById("mortgage-calculator");
-const total = document.getElementById("total");
+const totalTextContent = document.getElementById("total");
 
 mortgageCalculator.addEventListener("submit", function (event) {
+  // Don't reset the form on submit
   event.preventDefault();
-  const principalAmount = principal.value;
-  const monthlyInterestRate = interest.value / 100 / 12;
-  const totalPayments = length.value * 12;
 
-  let monthlyMortgagePayment =
-    (monthlyInterestRate * principalAmount) /
-    (1 - (1 + monthlyInterestRate) ** -totalPayments);
+  // If principal or length are 0, don't waste the cycles
+  if (principal.value == 0 || lengthOfLoan.value == 0) {
+    return;
+  }
+
+  // Now we can work with something
+  const principalAmount = principal.value;
+  const monthlyInterestRate = interestRate.value / 100 / 12;
+  const totalPayments = lengthOfLoan.value * 12;
+  let monthlyMortgagePayment = 0;
+
+  if (interestRate.value == 0) {
+    monthlyMortgagePayment = principalAmount / totalPayments;
+  } else {
+    monthlyMortgagePayment =
+      (monthlyInterestRate * principalAmount) /
+      (1 - (1 + monthlyInterestRate) ** -totalPayments);
+  }
+
   monthlyMortgagePayment = monthlyMortgagePayment.toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
     maximumFractionDigits: 0,
   });
-  total.textContent = monthlyMortgagePayment;
+  totalTextContent.textContent = monthlyMortgagePayment;
 });
